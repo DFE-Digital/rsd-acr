366dfb5dadbeebdc3f495d8a1d8dedb701d10749
Chris Wright <chris@dxw.com>
2024-04-11 15:51:03 +0100

    Allow adding allowed IP ranges to ACR
    * Creates network rules, allowing the given list of IP ranges to access
      the ACR

--- locals.tf
+++ locals.tf
@@ -8,10 +8,11 @@ locals {
   key_vault_access_ipv4 = var.key_vault_access_ipv4
   tfvars_filename       = var.tfvars_filename
 
-  registry_sku                   = var.registry_sku
-  registry_admin_enabled         = var.registry_admin_enabled
-  registry_public_access_enabled = var.registry_public_access_enabled
-  registry_retention_days        = var.registry_retention_days
+  registry_sku                       = var.registry_sku
+  registry_admin_enabled             = var.registry_admin_enabled
+  registry_public_access_enabled     = length(local.registry_network_allowed_ip_ranges) > 0 ? true : var.registry_public_access_enabled
+  registry_network_allowed_ip_ranges = var.registry_network_allowed_ip_ranges
+  registry_retention_days            = var.registry_retention_days
 
   private_endpoint_configutations = var.private_endpoint_configutations
 }
