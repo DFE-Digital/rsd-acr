a8940997458b814fa2a1aaab01dd176af17e7735
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-07-31 16:41:06 +0100

    Set up VNET and Subnet

--- vnet.tf
+++ vnet.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "default" {
   count = local.enable_agent_pool ? 1 : 0
 
-  name                = "${local.resource_prefix}vnet"
+  name                = "${local.resource_prefix}default"
   address_space       = ["172.16.0.0/12"]
   resource_group_name = azurerm_resource_group.acr.name
   location            = azurerm_resource_group.acr.location
@@ -14,5 +14,5 @@ resource "azurerm_subnet" "agent_pool" {
   name                 = "${local.resource_prefix}agentpoolinfra"
   virtual_network_name = azurerm_virtual_network.default[0].name
   resource_group_name  = azurerm_resource_group.acr.name
-  address_prefixes     = [cidrsubnet("172.16.0.0/12", 19, 0)]
+  address_prefixes     = ["172.16.0.0/29"]
 }
